13c05297fdaadec91f6a860916606d787551c217
Gordon Tillman <gtillman@zimbra.com>
2016-03-18 11:59:01 -0500

    updated project dependency versions

--- project.clj
+++ project.clj
@@ -3,26 +3,26 @@
   :url "http://zimbra.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/data.json "0.2.6"]
-                 [ring/ring-core "1.3.2"]
-                 [ring/ring-jetty-adapter "1.3.2"]
-                 [compojure "1.3.2"]
-                 [ring/ring-json "0.3.1"]
-                 [clj-http "1.0.1"]
-                 [clj-http-fake "1.0.1"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.5.0"]
+                 [ring/ring-json "0.4.0"]
+                 [clj-http "1.1.2"]
+                 [clj-http-fake "1.0.2"]
                  [log4j/log4j "1.2.17"]
                  [org.apache.commons/commons-daemon "1.0.9"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.374"]
                  ;; the following two entries are to support sqlite
                  [org.clojure/java.jdbc "0.3.7"]
                  [org.xerial/sqlite-jdbc "3.8.9"]]
   :main ^:skip-aot zimbra.simioj.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[ring/ring-mock "0.2.0"]
-                                  [clj-http-fake "1.0.1"]]
-                   :plugins [[lein-kibit "0.0.8"]]}}
+             :dev {:dependencies [[ring/ring-mock "0.3.0"]
+                                  [clj-http-fake "1.0.2"]]
+                   :plugins [[lein-kibit "0.1.2"]]}}
   :aot :all)
