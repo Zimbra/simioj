59ef27f0d0402358357ea74b06cb8104dfd3dc3b
Gordon Tillman <gtillman@zimbra.com>
2016-04-12 11:18:37 -0500

    wip - endpoint

--- src/zimbra/simioj/endpoint/http.clj
+++ src/zimbra/simioj/endpoint/http.clj
@@ -1,5 +1,26 @@
 (ns zimbra.simioj.endpoint.http
   (:require
    [clojure.core.async :refer [chan <!! >!! close!]]
-   [clojure.tools.logging :as logger])
+   [clojure.tools.logging :as logger]
+   [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
+
+
+;; (defn start! [ctx]
+;;   "Given the context CTX, which is wrapped in a ref, start the http endpoint.
+;; Will update CTX by adding an :endpoint instance."
+;;   (let [server (get-in [:endpoint :server] @ctx)]
+;;     (if server
+;;       (.start server)
+;;       (let [http-endpoint (get-in [:config :node :endpoint :http] @ctx)
+;;             host-port (.split http-endpoint ":")
+;;             host (first host-port)
+;;             port (Integer/parseInt (second host-port))]
+;;         (run-jetty (endpoint-app ctx) {:host host :port port :join? false})))))
+
+;; (defn stop
+;;   "Stop the node."
+;;   []
+;;   (when (-server)
+;;     (.stop (-server)))
+;;   @endpoint-state)
--- src/zimbra/simioj/main.clj
+++ src/zimbra/simioj/main.clj
@@ -1,7 +1,101 @@
 (ns zimbra.simioj.main
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.tools.logging :as logging]
+            [zimbra.simioj.config :as config])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
+
+(defn- ^:no-doc build-config [options]
+  (let [config-base (if (:replace-config options)
+                      (apply config/load-cfg-files! (clojure.string/split
+                                                     (:replace-config options) #","))
+                      config/default-config)
+        config-final (if (:update-config options)
+                       (config/load-config-maps!
+                        config-base
+                        (apply
+                         config/load-cfg-files! (clojure.string/split
+                                                 (:update-config options) #",")))
+                       config-base)]
+    config-final))
+
+
+;; (defn- ^:no-doc -service-start [ctx]
+;;   "Given the curent context CTX (which is wrapped in a ref),
+;; start the registered services"
+;;   (when (some identity (list [
+
+
+(def cli-options
+  [["-p" "--port PORT" "Port to listen for API."
+    :default (last (clojure.string/split (get-in
+                                          config/default-config
+                                          [:node :endpoint :http]) #":"))
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-u" "--update-config FILE1,FILE2,..." "update default config"]
+   ["-r" "--replace-config FILE1,FILE2,..." "replace default config"]
+   ["-h" "--help" "Print this help."]])
+
+
+(defn error-msg [errors]
+  (str "Command line arguments are invalid:\n\n"
+       (string/join \newline errors)))
+
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn start
+  "Handles the 'start' command"
+  [options]
+  (let [config (build-config options)
+        rpc {} ; TODO
+        raft {} ; TODO
+        ctx (ref {:config config
+                  :rpc rpc
+                  :raft raft})]
+    ;; TODO - start services
+    (clojure.pprint/pprint @ctx)))
+
+
+(defn usage [options-summary]
+  (string/join
+   \newline
+   ["SIMIOJ - Distributed Task Execution Framework."
+    ""
+    "Usage: program-name [options] action"
+    ""
+    "Options:"
+    options-summary
+    ""
+    "Default config loaded from"
+    config/default-config-file
+    ""
+    "Actions:"
+    "  start    Start the service"
+    ""
+    "Once started, the service will run until it is terminated."]))
+
+
+(defn ^:no-doc -main
+  "Provide support for running via command line with arguments.
+  Run as follows:
+
+      lein run
+      lein run -- [ARGS]
+
+  `lein run -- --help` will print full help message with all supported options.
+  "
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options) (exit 0 (usage summary))
+      (not= (count arguments) 1) (exit 1 (usage summary))
+      errors (exit 1 (error-msg errors)))
+
+    (case (first arguments)
+      "start" (start options)
+      (exit 1 (usage summary)))))
